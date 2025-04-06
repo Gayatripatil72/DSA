fe16ae6178c2f0bfe6800cf504478f91d10e90d8
Gayatripatil72 <gayatripatil18200411@gmail.com>
2025-04-06 20:03:33 +0530

    Add big_O.py

---
+++ big_O.py
@@ -0,0 +1,22 @@
+import time
+
+'''def get_sq_numbers(numbers):
+    sq_numbers = []
+    for n in numbers():
+        sq_numbers.append(n*n)
+    return sq_numbers
+
+numbers = list(range(1,100000))
+start_time =time.time()
+
+get_sq_numbers(numbers)
+
+end_time =time.time()
+
+print(f"Execution time:{end_time - start_time :.2f} seconds")'''
+
+
+
+#This code describes that the the how much time the code will get for execution .Using the Big O notation whichthe concept present in DSA .
+#Here 6f is used for getting atleat 6 digits after decimal points.we can take any number as f according to our need.
+
