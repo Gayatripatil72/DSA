c0cb0189bb1507b707e79fff874b5ddfdeb2e8b6
Gayatripatil72 <gayatripatil18200411@gmail.com>
2025-04-09 21:33:58 +0530

    Updated array.py with more examples

--- array.py
+++ array.py
@@ -8,9 +8,8 @@
 import array
 arr = array.array('i',[23, 45 , 7, 89, 67,8 ,0])
 
-'''for i in arr:
-    print(i)'''
-
+for i in arr:
+    print(i)
 
 #Accesing elments by indexing
 
@@ -26,5 +25,37 @@ print("After appending array:",arr)'''
 #adding element to specific index using insert method .
 #Syntax : array_name.insert(index ,element)
 
-arr.insert(2 , 25)
-print("Array after adding :", arr)
+'''arr.insert(2 , 25)
+print("Array after adding :", arr)'''
+
+#removing an element
+#Syntax : array_name.remove(element)
+
+'''arr.remove(25)
+print(arr)
+'''
+#by poping we can delete specific index number or last number
+#syntax:array_name.pop()  -> for last element
+#array_name.pop(1) -> for perticular element
+
+'''arr.pop()
+arr.pop(4)
+print(arr)'''
+
+#by using reverse we can reverse the array
+
+'''arr.reverse()
+print(arr)'''
+
+#converting array to list
+'''l1 = arr.tolist()
+print(l1)'''
+
+#we can add array over the existing array using extend function
+
+'''arr1 = array.array('i',[70 , 80])
+arr.extend(arr1)
+print(arr)'''
+
+print("Buffer info:", arr.buffer_info())
+
